66424f2cbb273076d50a9354da356fc5a5dde94a
felixwebman <felixosman168@gmail.com>
2025-11-30 15:24:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+// Select all buttons
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    alert("Thank you! Product added to your cart.");
+  });
+});
 
